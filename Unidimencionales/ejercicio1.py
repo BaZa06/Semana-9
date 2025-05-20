4f0854d589444c8fc4e80e03cc123da2efc00fe5
BaZa06 <bazeledon@uamv.edu.ni>
2025-05-20 11:40:51 -0600

    Se realioz un archivo a la salida

---
+++ Unidimencionales/ejercicio1.py
@@ -0,0 +1,10 @@
+#Cargar una lista manualmente
+array= []
+
+while True:
+    array = input("Ingrese un número (a ´fin´ para terminar): ")
+    if array < 0:
+        print("El número es entero")
+    else:
+        print("El número no es entero:" )
+print ("El número es:", array_int)
