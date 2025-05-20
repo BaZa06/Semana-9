6b865681bd850071600b77338df5cc2766d98e44
BaZa06 <bazeledon@uamv.edu.ni>
2025-05-20 08:37:28 -0600

    Se realizo un archivo a la salida

--- arreglo3.py
+++ arreglo3.py
@@ -5,3 +5,14 @@ print("Array de cadenas:", array_str)
 array_str.insert(3, "cero")
 print("Array de cadenas después de insertar ´cero´ al inicio:", array_str)
 
+#Contar cuantos elementos hay en el arreglo
+cantidad = len(array_str)
+print("Cantidad de elementos en el arreglo:", cantidad)
+
+#Eliminar un elemento del arreglo
+array_str.remove("tres")
+print("Array de cadenas después de eliminar ´tres´:", array_str)
+
+#Eliminar un elemento del arreglo por posición
+array_str.pop(2)
+print("Array de cadenas después de eliminar el elemento en la posición 2:", array_str)
